ece023229af911262908479b1fdf4026242f6253
Marcel <ma-wendler@outlook.com>
2023-01-14 21:31:24 +0100

    Add "getDependencyTree" to NpmRunner

--- src/NpmRunner.ts
+++ src/NpmRunner.ts
@@ -1,5 +1,21 @@
 import { exec, ExecException } from 'node:child_process';
 
+interface NpmPackageList {
+	version: string,
+	name: string,
+	dependencies: NpmPackageListEntries,
+}
+
+interface NpmPackageListEntries {
+	[packageName: string]: NpmPackageListEntry;
+}
+
+interface NpmPackageListEntry {
+	version: string,
+	resolved: string,
+	overridden: boolean,
+}
+
 export class NpmRunner {
 	runNpmCommand(command: string): Promise<string> {
 		return new Promise((resolve, reject) => {
@@ -19,6 +35,26 @@ export class NpmRunner {
 
 	getNpmVersion(): Promise<string> {
 		return this.runNpmCommand('-v');
+	}
+
+	getDependencyTree(depth: number = 0, onlyTopLevel: boolean = false, includeDev: boolean = false): Promise<NpmPackageListEntries> {
+		return new Promise((resolve, reject) => {
+			let command = 'ls --json --depth=' + depth; // List dependencies in JSON format up to configured depth.
+			if (!onlyTopLevel) {
+				command += ' --all'; // Include all dependencies (not only top-level).
+			}
+			if (!includeDev) {
+				command += ' --omit=dev'; // Exclude dev dependencies.
+			}
+
+			this.runNpmCommand(command)
+				.then((output) => {
+					const outputObject: NpmPackageList = JSON.parse(output);
+
+					resolve(outputObject.dependencies || {});
+				})
+				.catch((error) => reject(error));
+		});
 	}
 
 	private runNpmProcess(npmCommand: string): Promise<{stdout: string, stderr: string}> {
